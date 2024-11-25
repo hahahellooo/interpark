bd168ff2612f479472e186a7f5fc73d2f4d1668d
hahahellooo <hahahello777@gmail.com>
2024-11-25 17:15:14 +0900

    .

--- src/interpark/open_page_url.py
+++ src/interpark/open_page_url.py
@@ -12,6 +12,7 @@ def get_open_page_url(base_num,max_pages):
     # ChromePtions 객체 생성
     options = Options()
     options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--ignore-ssl-errors=yes")
