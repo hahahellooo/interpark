5949eba1a3811848c7628a11dd3ce83a46916ddc
hahahellooo <hahahello777@gmail.com>
2024-11-25 12:30:24 +0900

    ready to send

--- src/interpark/raw_open_page.py
+++ src/interpark/raw_open_page.py
@@ -27,10 +27,8 @@ def extract_open_html():
     # 크롤링 대상 URL
     open_page_lists = get_open_page_url(49300, 1)
 
-    # 결과 저장 경로 설정
-    save_dir = os.path.join("interpark")
-    os.makedirs(save_dir, exist_ok=True)  # 디렉토리 생성 (이미 존재하면 무시)
-
+    num = ''
+    crawling_list=[]
     try:
         for page in open_page_lists:
             print(f"{page} 접속 중...")
@@ -38,33 +36,31 @@ def extract_open_html():
 
             # URL에서 num 값 추출
             num = page.split("groupno=")[1].split("&")[0]
+            
+            try:
+                # `notice_detail` 클래스 요소가 로드될 때까지 대기
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "notice_detail"))
+                )
 
-            # `notice_detail` 클래스 요소가 로드될 때까지 대기
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "notice_detail"))
-            )
-
-            # `notice_detail` 클래스 요소 찾기
-            notice_detail_element = driver.find_element(By.CLASS_NAME, "notice_detail")
-            # 내부 HTML 가져오기
-            inner_html = notice_detail_element.get_attribute("outerHTML")
-            # BeautifulSoup으로 HTML 포맷팅
-            soup = BeautifulSoup(inner_html, "html.parser")
-            open_html = soup.prettify()
-
-            # HTML 파일로 저장
-            file_name = f"{num}.html"
-            file_path = os.path.join(save_dir, file_name)
-            with open(file_path, "w", encoding="utf-8") as file:
-                file.write(open_html)
-
-            print(f"{file_name} 저장 완료!")
+                # `notice_detail` 클래스 요소 찾기
+                notice_detail_element = driver.find_element(By.CLASS_NAME, "notice_detail")
+                # 내부 HTML 가져오기
+                inner_html = notice_detail_element.get_attribute("outerHTML")
+                # BeautifulSoup으로 HTML 포맷팅
+                soup = BeautifulSoup(inner_html, "html.parser")
+                crawling_list.append({"data":soup, "num":num})
+            except Exception as e:
+                print(f"html 추출 중 오류 발생: {e}")
+                continue
 
     except Exception as e:
-                print(f"공지페이지 접속 에러 : {e}")
+                print(f"오픈공지 접속 에러 : {e}")
 
     finally:
           driver.quit()
+
+    return crawling_list
                                
 # 함수 실행
 extract_open_html()
