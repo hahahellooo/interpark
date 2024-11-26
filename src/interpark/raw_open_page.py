9087526725d9ec572836139cb453433fb1d8d1c5
hahahellooo <hahahello777@gmail.com>
2024-11-26 09:07:24 +0900

    try 50

--- src/interpark/raw_open_page.py
+++ src/interpark/raw_open_page.py
@@ -27,7 +27,7 @@ def extract_open_html():
     # 크롤링 대상 URL
     #open_page_lists = get_open_page_url(49546,500)
     #open_page_lists = get_open_page_url(53403,2)
-    open_page_lists = get_open_page_url(49546,20)
+    open_page_lists = get_open_page_url(49566,50)
     
     num = ''
     crawling_list=[]
