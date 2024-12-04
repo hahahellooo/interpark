e27279faf5c48793c62cbc28912c5cae078c2970
hahahellooo <hahahello777@gmail.com>
2024-12-04 12:43:43 +0900

    try 50

--- src/interpark/read_s3_parsing.py
+++ src/interpark/read_s3_parsing.py
@@ -126,7 +126,7 @@ def html_parsing():
     hook = S3Hook(aws_conn_id=aws_conn_id)
 
     base_file_number = 52800  # 시작 파일 번호
-    end_file_number = base_file_number + 70  # 끝 파일 번호 설정
+    end_file_number = base_file_number +50  # 끝 파일 번호 설정
     # 파일 번호를 하나씩 증가시키면서 반복 처리
     #while True:###################################################테스트
     while base_file_number <= end_file_number:
