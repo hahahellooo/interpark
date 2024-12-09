f7e247b301c3c609121fbbfa238c718b5e67f695
hahahellooo <hahahello777@gmail.com>
2024-12-09 17:28:49 +0900

    raw open and ticket page

--- src/interpark/raw_open_page.py
+++ src/interpark/raw_open_page.py
@@ -27,7 +27,7 @@ def extract_open_html():
 
     # 크롤링 대상 URL
     #open_page_lists = get_open_page_url(49609,100)
-    open_page_lists = get_open_page_url(53208,2)
+    open_page_lists = get_open_page_url(53532,500)
     
     num = ''
     crawling_list=[]
